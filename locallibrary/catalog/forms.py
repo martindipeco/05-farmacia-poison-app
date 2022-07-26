28a2640a641f1dc17bc3524a69949b942a6c297e
Martin Di Peco <martindipeco@gmail.com>
2022-07-26 11:52:40 -0300

    Delete locallibrary directory

--- locallibrary/catalog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import fields
-
-from .models import Listado
-
-class ListadoForm(forms.ModelForm):
-    class Meta:
-        model = Listado
-        fields = '__all__'
